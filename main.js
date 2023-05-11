3fe8b8c6a772bedd8fc812960c9b4a4a75d5b682
AnandaBintang <anandabintang4@gmail.com>
2023-05-11 16:01:39 +0700

    preloader responsive fix

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-$("body").css("overflowY", "hidden");
-$("#preloader").css({
-  visibility: "visible",
-  opacity: "1",
-  transition: "opacity 2s linear",
-});
+$(document).ready(function () {
+  const mediaQuery = window.matchMedia("(min-width: 1025px)");
 
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    for (let i = 0; i <= 800; i++) {
-      setTimeout(() => {
-        $("#preloader").css({
-          bottom: `-${i}px`,
-          transition: "bottom 1s",
-        });
-      }, 500);
-    }
+  $("body").css("overflowY", "hidden");
+  $("#preloader").css({
+    visibility: "visible",
+    opacity: "1",
+    transition: "opacity 2s linear",
+  });
 
+  window.addEventListener("load", () => {
     setTimeout(() => {
-      $("body").css("overflowY", "auto");
-    }, 1300);
-  }, 2000);
+      for (let i = 0; i <= 800; i++) {
+        setTimeout(() => {
+          $("#preloader").css({
+            bottom: `-${i}px`,
+            transition: "bottom 1s",
+          });
+        }, 500);
+      }
+
+      setTimeout(() => {
+        $("body").css("overflowY", "auto");
+        if (!mediaQuery.matches) {
+          $(".navbar").addClass("sticky-top");
+        }
+      }, 1300);
+    }, 2000);
+  });
 });
